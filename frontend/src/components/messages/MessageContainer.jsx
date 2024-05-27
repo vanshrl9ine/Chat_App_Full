75e645cd8a88bb91afc0925bba6eff331307743e
Vansh <gu[ptavansh67@gmail.com>
2024-05-27 12:58:10 +0530

    commit

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -2,6 +2,7 @@ import MessageInput from "./MessageInput";
 import Messages from "./Messages";
 import { TiMessages } from "react-icons/ti";
 const MessageContainer = () => {
+    
 	return (
 		<div className='md:min-w-[450px] flex flex-col'>
 			<>
